1738087e4475150e1beb0fccb4ddd9db645b9a3e
Wenfeng Li <liwf6@lenovo.com>
2024-10-01 10:47:54 +0800

    invoke app

---
+++ WpfApp1/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public string FilePath { get; set; }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            if (e.Args.Length > 0)
+            {
+                FilePath = e.Args[0];
+
+            }
+
+            this.StartupUri = new Uri("MainWindow.xaml", UriKind.Relative);
+        }
+    }
+
+}
